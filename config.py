c7e048d72dded6baa25622ee9ba9ccb2f29f6add
scaldara55 <stephen@caldara.net>
2020-11-27 17:18:56 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+
+app_config = {
+    'camera': 'M5CAMERA',  # camera -> 'ESP32-CAM' or 'M5CAMERA'
+    'led': 14, # led -> 4: ESP32-CAM or 14: M5CAMERA
+}
+
+wifi_config = {
+    'ssid':'',
+    'password':''
+}
